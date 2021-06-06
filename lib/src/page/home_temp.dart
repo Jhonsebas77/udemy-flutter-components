e9c72d2b2f2eafae8e33e703ed9e183405baba12
Sebastian Otalora <jhonsebas77@outlook.com>
2021-06-06 16:21:39 -0500

    Implement other way to create a List

--- lib/src/page/home_temp.dart
+++ lib/src/page/home_temp.dart
@@ -18,25 +18,49 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componetes Temp'),
       ),
       body: ListView(
-        children: _buildItemsLarge(),
+        // children: _buildItemsLarge(),
+        children: _buildItemsShort(),
       ),
     );
   }
 
-  List<Widget> _buildItemsLarge() {
-    List<Widget> list = new List<Widget>();
-    for (String item in options) {
-      final _tempWidget = ListTile(
-        title: Text(
-          item,
-        ),
-      );
-      list
-        ..add(_tempWidget)
-        ..add(
-          Divider(),
+  // List<Widget> _buildItemsLarge() {
+  //   List<Widget> list = new List<Widget>();
+  //   for (String item in options) {
+  //     final _tempWidget = ListTile(
+  //       title: Text(
+  //         item,
+  //       ),
+  //     );
+  //     list
+  //       ..add(_tempWidget)
+  //       ..add(
+  //         Divider(),
+  //       );
+  //   }
+  //   return list;
+  // }
+
+  List<Widget> _buildItemsShort() => options.map((item) {
+        return Column(
+          children: [
+            ListTile(
+              title: Text(
+                '-> $item',
+              ),
+              subtitle: Text(
+                '${item.length}',
+              ),
+              leading: Icon(
+                Icons.account_circle_sharp,
+              ),
+              trailing: Icon(
+                Icons.keyboard_arrow_right_outlined,
+              ),
+              onTap: () => print(item),
+            ),
+            Divider(),
+          ],
         );
-    }
-    return list;
-  }
+      }).toList();
 }
