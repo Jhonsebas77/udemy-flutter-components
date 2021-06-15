e35ec8bc6f2624cbd7e78dd8e5591047a6f483d3
Sebastian Otalora <jhonsebas77@outlook.com>
2021-06-15 13:17:01 -0500

    Fix Slider Icons

--- lib/src/utils/icon_string_util.dart
+++ lib/src/utils/icon_string_util.dart
@@ -6,7 +6,7 @@ final _icons = <String, IconData>{
   'folder_open': Icons.folder_open,
   'play_circle_fill': Icons.play_circle_fill,
   'input': Icons.input,
-  'sliders': Icons.list,
+  'sliders': Icons.tune,
 };
 
 Icon getIcon(String nameIcon) => Icon(
